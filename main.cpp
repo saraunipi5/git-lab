03b4e65cc7dfc759efe73e3ca8ac7825fabc46c5
Sara <sara.unipi5@gmail.com>
2026-02-04 17:15:21 +0100

    modifica da feature-online

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
 HEAD
     cout << "Ciao dal main branch!" << endl;
 
-    cout << "Ciao da feature branch" << endl;
+    cout << "Versione inviata su GitHub dal feature branch" << endl;
  feature-saluto
     return 0;
 }
